854c5f400d2e83adff087d7ec955d8b0c7cf46c1
Rafagenaro <rafaelgenarovitor@gmail.com>
2025-04-09 20:58:45 -0300

    feat: listando conversas

--- chatPage.py
+++ chatPage.py
@@ -1,6 +1,13 @@
+import re
+from pathlib import Path
+import pickle
+
 import streamlit as st
 import openai
+from unidecode import unidecode
 
+PASTA_MENSAGENS = Path(__file__).parent / 'mensagens'
+PASTA_MENSAGENS.mkdir(exist_ok=True)
 openai_key = ''
 
 def return_resposta_modelo(mensagens, openai_key,modelo='gpt-3.5-turbo',temperatura = 0, stream=False):
@@ -8,12 +15,43 @@ def return_resposta_modelo(mensagens, openai_key,modelo='gpt-3.5-turbo',temperat
     response = openai.ChatCompletion.create(model=modelo, messages=mensagens, temperature=temperatura, stream=stream)
     return response
 
+def converte_nome_mensagem(nome_mensagem):
+    nome_arquivo = unidecode(nome_mensagem)
+    nome_arquivo = re.sub('\W+', '', nome_arquivo).lower()
+    return nome_arquivo
+
+def retorna_nome_da_mensagem(mensagens):
+    nome_mensagem = ''
+    for mensagem in mensagens:
+        if mensagem['role'] == 'user':
+            nome_mensagem = mensagem['content'][:30]
+            break
+    return nome_mensagem
+
+def salvar_mensagens(mensagens):
+    if len(mensagens) == 0:
+        return False
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    arquivo_salvar = {'nome_mensagem': nome_mensagem, 'nome_arquivo': nome_arquivo, 'mensagem': mensagens}
+    with open(PASTA_MENSAGENS / nome_arquivo, 'wb') as f:
+        pickle.dump(arquivo_salvar, f)
+
+def ler_mensagens(mensagens, key='mensagem'):
+    if len(mensagens) == 0:
+        return []
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
+        mensagens = pickle.load(f)
+    return mensagens[key]
+
 def main_page():
 
     if not 'mensagens' in st.session_state:
         st.session_state.mensagens = []
 
-    mensagens = st.session_state['mensagens']
+    mensagens = ler_mensagens(st.session_state['mensagens'])
 
 
     st.header('Genaro AI', divider=True)
@@ -49,5 +87,6 @@ def main_page():
 
 
         st.session_state['mensagens'] = mensagens
+        salvar_mensagens(mensagens)
 
 main_page()
